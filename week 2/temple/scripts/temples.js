67da43e62edd75383800722eb9767a294e7b1cc3
Roguin <ro.guin31@gmail.com>
2025-01-19 08:27:36 -0400

    update

--- week 2/temple/scripts/temples.js
+++ week 2/temple/scripts/temples.js
@@ -3,3 +3,35 @@ window.onload = function () {
   document.getElementById("lastModified").textContent =
     "Last modified: " + document.lastModified;
 };
+
+const links = [
+  { href: "#", text: "Home" },
+  { href: "#", text: "Old" },
+  { href: "#", text: "New" },
+  { href: "#", text: "Large" },
+  { href: "#", text: "Small" },
+];
+const createNav = (links) => {
+  const ul = document.createElement("ul");
+  links.forEach((link) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<a href="${link.href}">${link.text}</a>`;
+    ul.appendChild(li);
+  });
+  return ul;
+};
+const nav = document.querySelector("nav");
+nav.appendChild(createNav(links));
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector("nav ul");
+
+  // Toggle menu and icon
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+    hamburger.innerHTML = navMenu.classList.contains("active")
+      ? "&#10006;"
+      : "&#9776;";
+  });
+});
