3220245728028b490266d085fda9c08594b52569
Roguin <ro.guin31@gmail.com>
2025-01-20 10:12:57 -0400

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,4 +2,5 @@ window.onload = function () {
   document.getElementById("currentyear").textContent = new Date().getFullYear();
   document.getElementById("lastModified").textContent =
     "Last modified: " + document.lastModified;
+  console.log(document.getElementById(`lastModified`).textContent);
 };
